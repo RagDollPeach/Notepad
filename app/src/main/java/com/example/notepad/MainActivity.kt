0080ad9cef003c852595f43d4b98cf28cb8b35db
RagDollPeach <87299988+RagDollPeach@users.noreply.github.com>
2022-09-11 15:30:02 +0300

    second commit

--- app/src/main/java/com/example/notepad/MainActivity.kt
+++ app/src/main/java/com/example/notepad/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.notepad
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.notepad.databinding.ActivityMainBinding
+import com.example.notepad.view.fragments.NotesFragment
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, NotesFragment.newInstance())
+                .commitNow()
+        }
     }
 }
---
+++ app/src/main/java/com/example/notepad/model/Note.kt
@@ -0,0 +1,4 @@
+package com.example.notepad.model
+
+data class Note(val title: String, val text: String, val date: String)
+
